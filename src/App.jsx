7a89206d5d65d71c431e275749ee7e02e6b6c39e
Igor Solfa Romero <igorsolfaromero@gmail.com>
2021-10-07 11:38:15 -0300

    Criando Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,12 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Home from './paginas/Home'
 import Sobre from './paginas/Sobre'
 import Pagina404 from './paginas/Pagina404'
+import Cabecalho from './components/Cabecalho'
 
 function App() {
   return (
     <Router>
+      <Cabecalho />
       <Switch>
         <Route exact path="/">
           <Home />
