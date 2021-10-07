c5817d7ee81024ad720c24ae8745e136ef7e4947
Igor Solfa Romero <igorsolfaromero@gmail.com>
2021-10-07 11:22:52 -0300

    Erro 404

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 import Home from './paginas/Home'
 import Sobre from './paginas/Sobre'
+import Pagina404 from './paginas/Pagina404'
 
 function App() {
   return (
@@ -14,6 +15,9 @@ function App() {
         </Route>
         <Route path="/sobre">
           <Sobre />
+        </Route>
+        <Route>
+          <Pagina404 />
         </Route>
       </Switch>
     </Router>
