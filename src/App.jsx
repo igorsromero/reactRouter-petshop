b7ce461644268ce8b154275054360201708b49a7
Igor Solfa Romero <igorsolfaromero@gmail.com>
2021-10-07 10:53:03 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './assets/css/base/base.css'
+import Home from './paginas/Home'
+import Sobre from './paginas/Sobre'
+
+function App() {
+  return (
+    <>
+      <Home />
+      <Sobre />
+    </>
+  )
+}
+
+export default App
